3785741c6f59ec50a4c782814655fe4b16b37d2c
casa <sacadmedeperu@gmail.com>
2022-11-22 20:53:52 +0100

    backupdatos

--- app/src/main/java/com/manumegia/ejecicio1progmultimedia/MainActivity.kt
+++ app/src/main/java/com/manumegia/ejecicio1progmultimedia/MainActivity.kt
@@ -2,6 +2,7 @@ package com.manumegia.ejecicio1progmultimedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     private var onCreate = 0
@@ -12,10 +13,17 @@ class MainActivity : AppCompatActivity() {
     private var onStop = 5
     private var onDestroy = 6
 
+
+    private lateinit var contenido: EditText
+    private lateinit var contenido2: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("onCreate $onCreate")
+
+        contenido = findViewById(R.id.editText)
+        contenido2 = findViewById(R.id.editText2)
     }
 
     override fun onStart() {
@@ -46,5 +54,20 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         println("onDestroy $onDestroy")
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putString("Nombre", contenido.text.toString())
+        savedInstanceState.putString("Apellido", contenido2.text.toString())
+
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        contenido.setText(savedInstanceState.getString("Nombre"))
+        contenido2.setText(savedInstanceState.getString("Apellido"))
+
     }
 }
